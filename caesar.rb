47c91c1714337623cb0f3270cef513366e6fa895
Hayri <hello@hoshilab.com>
2014-09-16 00:07:58 +0200

    updated the file to make it shift chars

--- caesar.rb
+++ caesar.rb
@@ -1,13 +1,27 @@
 def cipher(string, shift)
-  my_message = []
-  my_message = string.split('')
+  my_message = ''
+  
+  string.each_char do |i|
+    my_message += (i.ord + shift).chr
+  end
 
-  print my_message
-  puts "-" * 10
-  print my_message.rotate(shift)
+  puts my_message
 end
+
+def cipher_decript(string, shift)
+  my_message = ''
+
+  string.each_char do |i|
+    my_message += (i.ord - shift).chr
+  end
+
+  puts my_message
+end
+
 
 puts "Enter message"
 get_message = gets.chomp
+get_shift_num = gets.chomp.to_i
 
-cipher(get_message,3)
+cipher(get_message,get_shift_num)
+cipher_decript(get_message,get_shift_num)
