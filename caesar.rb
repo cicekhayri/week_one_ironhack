c6943e0f2fdb91bb7cbbc287c207c427f2d878e0
Hayri <hello@hoshilab.com>
2014-09-16 21:24:20 +0200

    added the alphabet array

--- caesar.rb
+++ caesar.rb
@@ -1,18 +1,20 @@
 def cipher(string, shift)
+  alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
   my_message = ''
   
   string.each_char do |i|
-    my_message += (i.ord + shift).chr
+    my_message += alphabet[(alphabet.index(i.downcase) + shift) % alphabet.length]
   end
 
   puts my_message
 end
 
 def cipher_decript(string, shift)
+  alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
   my_message = ''
 
   string.each_char do |i|
-    my_message += (i.ord - shift).chr
+    my_message += alphabet[(alphabet.index(i.downcase) - shift) % alphabet.length]
   end
 
   puts my_message
